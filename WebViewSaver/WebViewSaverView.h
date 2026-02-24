5acd911eb3e1eba5af03ae9ecc6c104dccf0f191
dtinth on MBP M1 <dtinth@spacet.me>
2026-02-24 18:36:41 +0700

    Initial Commit

---
+++ WebViewSaver/WebViewSaverView.h
@@ -0,0 +1,12 @@
+//
+//  WebViewSaverView.h
+//  WebViewSaver
+//
+//  Created by Thai Pangsakulyanont on 2026-02-24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface WebViewSaverView : ScreenSaverView
+
+@end
---
+++ WebViewSaver/WebViewSaverView.m
@@ -0,0 +1,51 @@
+//
+//  WebViewSaverView.m
+//  WebViewSaver
+//
+//  Created by Thai Pangsakulyanont on 2026-02-24.
+//
+
+#import "WebViewSaverView.h"
+
+@implementation WebViewSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
